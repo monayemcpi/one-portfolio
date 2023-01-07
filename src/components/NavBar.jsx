2dbfc4c7f133f9d8717e92c28a4ef90bb66d43c6
monayemcpi <monayemcpi@gmail.com>
2023-01-07 19:18:53 +0600

    Update Navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+const NavBar = () => {
+
+    const links =[
+        {
+            id:1,
+            link:'home'
+        },
+        {
+            id:2,
+            link:'about'
+        },
+        {
+            id:3,
+            link:'portfolio'
+        },
+        {
+            id:4,
+            link:'experience'
+        },
+        {
+            id:5,
+            link:'contact'
+        },
+    ]
+
+
+  return (
+    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
+       <div>
+            <h1 className="text-5xl font-signature ml-2">Yash</h1>
+        </div> 
+        <ul className="flex">
+
+            {
+                links.map(link =>(
+
+                    <li key={link.id} className="px-4 cursor-pointer capitalize font-medium
+                    text-gray-500 hover:scale-105 duration-200">
+                    {link.link}</li> 
+
+                ))
+            }
+
+
+
+        </ul>
+    </div>
+  )
+}
+
+export default NavBar
